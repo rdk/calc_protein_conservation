dbb8a7fa99e5f1c39e80e16c0f799cdbc43207e2
rdk <rdk@users.noreply.github.com>
2018-03-23 23:05:25 +0100

    setenv

---
+++ setenv.sh
@@ -0,0 +1,22 @@
+
+# copy this file to local-setenv.sh and edit
+
+# edit this
+
+PSIBLAST_DIR=/mnt/ssd/conservation/conservation-workbench/ncbi-blast/bin
+CDHIT_DIR=/mnt/ssd/conservation/conservation-workbench/cd-hit
+MUSCLE_DIR=/mnt/ssd/conservation/conservation-workbench
+
+JSD_DIR=/mnt/ssd/conservation/conservation-workbench/conservation_code
+PYTHON_CMD=python
+
+BLASTDB_DIR=/mnt/ssd/conservation/blastdb
+
+
+# leave this
+
+export PATH=$PSIBLAST_DIR:$CDHIT_DIR:$MUSCLE_DIR:$PATH
+export JSD_DIR=$JSD_DIR
+export BLASTDB=$BLASTDB_DIR
+export PYTHON_CMD=$PYTHON_CMD
+
